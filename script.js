ab075c1a0e14d72047b706d8321a6f4951659c31
nathanielbd <36861707+nathanielbd@users.noreply.github.com>
2019-03-17 19:42:05 -0500

    Added scrolling header and network images

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onscroll = function headerScroll() {
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
