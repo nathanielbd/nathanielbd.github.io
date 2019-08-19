abad6bb5c6f35f972e327a3e2203da5b91fce00c
Nathaniel Budijono <nathanielbd@gmail.com>
2019-08-19 17:46:04 -0500

    removed unneeded files

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function headerScroll() {
-    var header = document.getElementById("header");
-    var sticky = header.offsetTop;
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-}
